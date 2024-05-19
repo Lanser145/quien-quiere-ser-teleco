8f6f063d382b9f9485df183bbc0d037400299429
Lanser145 <stirado13300@gmail.com>
2024-05-19 20:37:19 +0200

    Mi primera versión (sobre nosotros)

--- app/ui/fuentes.tsx
+++ app/ui/fuentes.tsx
@@ -1,7 +1,6 @@
 // Fuentes
-import { Lusitana, Orbitron, Kode_Mono } from 'next/font/google';
+import { Lusitana, Orbitron } from 'next/font/google';
  
 export const lusitana = Lusitana({ subsets: ['latin'], weight: "400"});
 export const orbitron = Orbitron({ subsets: ['latin'], weight: "400"});
-export const kode_mono = Kode_Mono({ subsets: ['latin'], weight: "400"});
 
